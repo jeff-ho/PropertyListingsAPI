6a14242bb0410b8cf5e17be4cca0f86f5a5f48c4
Jeff <jeff.ho@hotmail.com>
2023-07-25 17:24:07 -0400

    Minor refactoring

--- PropertyListingsApi/Services/ListingService/ListingService.cs
+++ PropertyListingsApi/Services/ListingService/ListingService.cs
@@ -5,12 +5,6 @@ namespace PropertyListingsApi.Services.ListingService
     public class ListingService : IListingService
     {
 
-        private static List<Listing> propertyListings = new List<Listing>
-           {
-               new Listing {Id=1, Title="Nice Place", Description="Super nice place", AgentName="John Smith", Address="123 Street", Bedrooms= 3, Bathrooms= 3, Price= 100000},
-               new Listing {Id=2, Title="Second Place", Description="Second Super nice place", AgentName="Jenny Smith", Address="312 Street", Bedrooms= 5, Bathrooms= 5, Price= 1800000},
-           };
-
         private readonly DataContext _context;
         public ListingService(DataContext context)
         {
